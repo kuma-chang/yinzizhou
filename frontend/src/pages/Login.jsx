0664c2692f4ec165e411566b4a78b97d64afd9c1
Kuma <micchang.kuma@gmail.com>
2025-07-08 01:29:16 -0500

    Add button for better routing

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,7 +1,27 @@
 import Form from "../components/Form"
+import { useNavigate } from "react-router-dom";
+import "../styles/Login.css"
+
+function ShowNoAccountButton () {
+    const navigate = useNavigate();
+    const handleNoAccount = () => {
+        navigate("/register")
+    };
+
+    return (
+        <button className="login-button" onClick={handleNoAccount}>
+            Don't have account
+        </button>
+    );
+}
 
 function Login() {
-    return <Form route="/api/token/" method="login" />
+    return (
+        <div>
+            <Form route="/api/token/" method="login" />
+            <ShowNoAccountButton />
+        </div>
+    );
 }
 
 export default Login
--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -1,7 +1,28 @@
 import Form from "../components/Form"
+import { useNavigate } from "react-router-dom";
+import "../styles/Register.css"
+
+function BackToLoginButton () {
+    const navigate = useNavigate();
+    const handleBackToLogin= () => {
+        navigate("/login")
+    };
+
+    return (
+        <button className="backtologin-button" onClick={handleBackToLogin}>
+            Back to Login
+        </button>
+    );
+}
+
 
 function Register() {
-    return <Form route="/api/user/register/" method="register" />
+    return (
+        <div>
+            <Form route="/api/user/register/" method="register" />
+            <BackToLoginButton />
+        </div>
+    );
 }
 
 export default Register
