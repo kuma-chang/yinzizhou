1f2b4b4856f1939d8d6f0b5e03c472eb874ea9ec
Kuma <micchang.kuma@gmail.com>
2025-07-08 01:28:25 -0500

    set up for ECS deployment

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,5 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+    plugins: [react()],
+    // Base name of your app
+    base: "/", // Replace this with the subdirectory path if needed
 })
